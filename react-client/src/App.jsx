f035f9595894a9a0b9783695cfb6fdefc63f0588
derekd03 <134988441+derekd03@users.noreply.github.com>
2025-11-03 12:02:48 -0800

    React client created

---
+++ react-client/src/App.jsx
@@ -0,0 +1,12 @@
+import ProfitChart from './components/ProfitChart'
+import './styles/ProfitChart.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ProfitChart />
+    </div>
+  )
+}
+
+export default App
