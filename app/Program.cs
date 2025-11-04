2ce9c67c985098819bb5fb88aa2dbfe7dcd39276
Bhavnoor Singh Saroya <bsaroya3@my.bcit.ca>
2025-11-03 17:14:42 -0800

    update for react

--- app/Program.cs
+++ app/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:5292")
+            .WithOrigins("http://localhost:5173")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
